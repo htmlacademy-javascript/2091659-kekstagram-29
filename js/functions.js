3f30bb9e059e5afc254782707aa92c9026727432
booblik2022 <booblik2022@gmail.com>
2023-06-14 18:09:12 +0300

    Добавил первую функцию проверки длины

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+//Функция для проверки длины строки.
+function comparingLength(string, length) {
+  return string.length <= length;
+}
+
+comparingLength('проверяемая строка', 20);
+
+console.log(comparingLength('проверяемая строка', 20));
+console.log(comparingLength('проверяемая строка', 18));
+console.log(comparingLength('проверяемая строка', 10));
+
+
+//Функция для проверки, является ли строка палиндромом
+function checkingЗalindrome() {}
